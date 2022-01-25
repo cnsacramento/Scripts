c9a05ef6660cdd2506cb6191a65ad702f7d4cd6c
Christian Novo Sacramento <christianivh366@gmail.com>
2022-01-25 22:32:02 +0000

    Añadido Script resta.sh

---
+++ EjerciciosScripts/resta.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# NOMBRE: resta.sh
+# OBJETIVO: Se recogen dos argumentos y devuelve su resta
+# AUTOR: Christian Novo Sacramento
+# FECHA: 25/01/2022
+
+# ENTRADAS(2): 1: Primer número que resta, 2: Segundo número de la resta
+# SALIDAS:
+
+# VERSIONES:
+
+
+#######################################################
+
+
+numero1=$1
+numero2=$2
+
+echo "El resultado de $numero1 - $numero2 es: " $(($numero1 - $numero2))
