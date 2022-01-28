a96d82b07e93ed50e474f5c7b37cc4b01d17b53a
cnsacramento <christianivh366@gmail.com>
2022-01-28 08:03:55 +0000

    Scripts en construcción

---
+++ EjerciciosScripts/comparar_numeros.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# NOMBRE: comparar_numero.sh
+# OBJETIVO: Compara con un condicional 2 números
+# AUTOR: Christian Novo Sacramento
+# FECHA: 27/01/2022
+
+# ENTRADAS(2): a: Número1, b: Número2
+# SALIDAS: Compara dos números con un condicional
+
+# VERSIONES: 1.0
+
+
+#######################################################
+
+
+
+a=1
+b=2
+
+echo $(($a - $b )
