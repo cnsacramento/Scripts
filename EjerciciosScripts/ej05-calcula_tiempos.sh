24d4a17a4107d7fee4b65259be263398d68ad052
Christian Novo Sacramento <christianivh366@gmail.com>
2022-03-01 10:59:02 +0000

    Se añade la correción de argumento ej05

--- EjerciciosScripts/ej05-calcula_tiempos.sh
+++ EjerciciosScripts/ej05-calcula_tiempos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# NOMBRE: calcula_tiempos.sh
+# NOMBRE: ej05-calcula_tiempos.sh
 # OBJETIVO:Convertir segundos a días,horas,minutos,segundo. 
 # AUTOR: Christian Novo Sacramento
 # FECHA: 26/01/2022
@@ -13,12 +13,18 @@
 #######################################################
 
 #Argumentos de Entrada
-segundos=$1
+SEGUNDOS=$1
+
+if [ $# -lt 1 ];
+then
+	echo "Parece que se te olvidó poner los segundos"
+	read -p "Segundos: " SEGUNDOS
+fi
 # 1dia = 86400segundos, 1hora = 3600segundos, 1min = 60segundos
-dias=$(($segundos/86400))
-modDias=$(($segundos%86400))
-horas=$(($modDias/3600))
-modHoras=$(($modDias%3600))
-minutos=$(($modHoras/60))
-modMinutos=$(($modHoras%60)) # El resto de los minutos son los segundos restantes
-echo "$segundos segundos son: $dias dias $horas horas $minutos minutos $modMinutos segundos"
+DIAS=$(($SEGUNDOS/86400))
+MOD_DIAS=$(($SEGUNDOS%86400))
+HORAS=$(($MOD_DIAS/3600))
+MOD_HORAS=$(($MOD_DIAS%3600))
+MINUTOS=$(($MOD_HORAS/60))
+MOD_MINUTOS=$(($MOD_HORAS%60)) # El resto de los minutos son los segundos restantes
+echo "$SEGUNDOS segundos son: $DIAS dias $HORAS horas $MINUTOS minutos $MOD_MINUTOS segundos"
